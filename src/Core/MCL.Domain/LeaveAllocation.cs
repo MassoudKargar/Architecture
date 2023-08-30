8aa7517142f2c8bd90db23670c0c254fc5f53cda
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2023-08-30 14:49:05 +0330

    Add project files.

---
+++ src/Core/MCL.Domain/LeaveAllocation.cs
@@ -0,0 +1,10 @@
+﻿using MCL.Domain.Common;
+
+namespace MCL.Domain;
+public class LeaveAllocation : BaseDomainEntity
+{
+    public int NumberOfDays { get; set; }
+    public LeaveType LeaveType { get; set; }
+    public int LeaveId { get; set; }
+    public int Priod { get; set; }
+}
---
+++ src/Core/MCL.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using MCL.Domain.Common;
+
+namespace MCL.Domain;
+public class LeaveType : BaseDomainEntity
+{
+    public string Name { get; set; }
+    public int DefaultDay { get; set; }
+}
+
