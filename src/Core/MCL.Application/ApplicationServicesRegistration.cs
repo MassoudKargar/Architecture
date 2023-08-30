827f60d939962c041139bf2cf6449f63626b4f3d
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2023-08-30 16:57:12 +0330

    Add Registration file

---
+++ src/Core/MCL.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,9 @@
+﻿namespace MCL.Application;
+public static class ApplicationServicesRegistration
+{
+    public static void ConfigureApplicationService(this IServiceCollection services)
+    {
+        //services.AddAutoMapper(typeof(MappingProfile));
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+}
--- src/Core/MCL.Application/GlobalUsings.cs
+++ src/Core/MCL.Application/GlobalUsings.cs
@@ -1,7 +1,9 @@
 // Global using directives
 
+global using System.Reflection;
 global using AutoMapper;
 global using MCL.Application.DTOs;
 global using MCL.Application.DTOs.Common;
 global using MCL.Application.DTOs.LeaveRequest;
 global using MCL.Domain;
+global using Microsoft.Extensions.DependencyInjection;
