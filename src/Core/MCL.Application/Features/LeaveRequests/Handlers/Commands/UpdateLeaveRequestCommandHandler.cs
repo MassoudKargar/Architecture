3ddb3d4c9cf63be55a21382373dab672731b2c83
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2023-08-30 21:32:56 +0330

    Change type Update LeaveRequest Command

--- src/Core/MCL.Application/Features/LeaveRequests/Handlers/Commands/UpdateLeaveRequestCommandHandler.cs
+++ src/Core/MCL.Application/Features/LeaveRequests/Handlers/Commands/UpdateLeaveRequestCommandHandler.cs
@@ -12,9 +12,9 @@ public class UpdateLeaveRequestCommandHandler : IRequestHandler<UpdateLeaveReque
     public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
     {
             var leaveRequest = await Repository.GetAsync(request.Id, cancellationToken);
-        if (request.LeaveRequestDto is not null)
+        if (request.UpdateLeaveRequestDto is not null)
         {
-            Mapper.Map(request.LeaveRequestDto, leaveRequest);
+            Mapper.Map(request, leaveRequest);
             await Repository.UpdateAsync(leaveRequest, cancellationToken);
         } else if (request.ChangeLeaveRequestApprovalDto is not null)
         {
--- src/Core/MCL.Application/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
+++ src/Core/MCL.Application/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
@@ -2,6 +2,6 @@
 public class UpdateLeaveRequestCommand : IRequest<Unit>
 {
     public int Id { get; set; }
-    public LeaveRequestDto LeaveRequestDto { get; set; }
+    public UpdateLeaveRequestDto UpdateLeaveRequestDto { get; set; }
     public ChangeLeaveRequestApprovalDto ChangeLeaveRequestApprovalDto { get; set; }
 }
