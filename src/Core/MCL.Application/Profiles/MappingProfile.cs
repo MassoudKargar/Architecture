16993685076a6866efe774379a6a7a6ee311606f
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2023-08-31 13:04:00 +0330

    Add Region in Profile AutoMaooer

--- src/Core/MCL.Application/Profiles/MappingProfile.cs
+++ src/Core/MCL.Application/Profiles/MappingProfile.cs
@@ -3,9 +3,22 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<LeaveType,LeaveTypeDto>().ReverseMap();
-        CreateMap<LeaveRequest,LeaveRequestDto>().ReverseMap();
-        CreateMap<LeaveRequest,LeaveRequestListDto>().ReverseMap();
-        CreateMap<LeaveAllocation,LeaveAllocationDto>().ReverseMap();
+        #region LeaveRequest
+        CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+        CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+        CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+        CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+        #endregion
+
+        #region LeaveAllocatuin
+        CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+        #endregion
+
+        #region LeaveType
+        CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+        #endregion
     }
 }
