8868643f35826c0f6bc0cc3b31e7b2375f156f5e
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2023-09-02 00:43:51 +0330

    Add Test Layer

---
+++ test/MCL.Application.NUnitTest/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using Microsoft.Playwright.NUnit;
+
+global using NUnit.Framework;
+
+global using System.Text.RegularExpressions;
+global using System.Threading.Tasks;
+global using MCL.Application.Contracts.Persistence;
+global using MCL.Domain;
+global using Moq;
